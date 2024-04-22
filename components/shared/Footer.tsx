5ab727675f1342d748cd19f36dda8139d06cf6bf
hendri <hendriansyah3105@gmail.com>
2024-04-22 19:20:55 +0700

    first commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+import  Link from "next/link"
+
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+        <Image src="/assets/images/" alt="logo" width={128} height={38}/>
+        </Link>
+        <p>2024 bergembira music</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
